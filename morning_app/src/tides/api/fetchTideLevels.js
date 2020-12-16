e2de0cda93704d10a58e6abb689c79678398db4d
katelynmiller <millerk9@wit.edu>
2020-12-15 21:20:09 -0500

    starting tide api work

---
+++ morning_app/src/tides/api/fetchTideLevels.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import fetch from 'fetcg'
+
+
+const stationID = 'ca0101';
+var currentTime = new Date().getTime();
+const baseUrl = "https://api.tidesandcurrents.noaa.gov/api/prod/datagetter";
+
+
+async function getTides() {
+    
+    const response = await fetch(url,)
+}

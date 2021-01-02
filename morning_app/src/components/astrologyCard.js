17c169b14d069316b5e27feb43c312011c487b2d
katelynmiller <millerk9@wit.edu>
2021-01-02 14:30:41 -0500

    Day 9 of 100: Astrology Card and api

--- morning_app/src/components/astrologyCard.js
+++ morning_app/src/components/astrologyCard.js
@@ -1,5 +1,18 @@
 import React, {Component} from 'react';
-import { CardContent,CardHeader ,Card, Typography } from '@material-ui/core';
+import { CardContent, CardHeader, Card, Typography } from '@material-ui/core';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+
 import tropicalTransistsApi from '../api/tropicalTransitsApi';
 
 class astrologyCard extends Component {
@@ -9,9 +22,14 @@ class astrologyCard extends Component {
             <Card>
                 <CardHeader>
                     <CardContent>
-                        <Typography>
-
-                        </Typography>
+                        <FormGroup>
+                            <List dense={dense}>
+                                <ListItem>
+                                    Moon is in : 
+                                </ListItem>
+                            </List>
+                                
+                        </FormGroup>
                     </CardContent>
             </CardHeader>
             </Card>
